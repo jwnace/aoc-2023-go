21d0c881205b98e9634adfe1bb07dc626993106d
Joe Nace <jwnace@gmail.com>
2023-12-16 21:10:48 -0500

    updated tests for day 3

--- day03/day03_test.go
+++ day03/day03_test.go
@@ -2,48 +2,96 @@ package day03
 
 import "testing"
 
-func TestPart1Example(t *testing.T) {
-	input := []string{
-		"",
-	}
-
-	expected := 0
-
-	actual := solve1(input)
-
-	if actual != expected {
-		t.Errorf("Part1() = %d, expected %d", actual, expected)
-	}
-}
-
 func TestPart1Solution(t *testing.T) {
-	expected := 0
-	actual := Part1()
-
-	if actual != expected {
-		t.Errorf("Part1() = %d, expected %d", actual, expected)
+	tests := []struct {
+		name string
+		want int
+	}{
+		{"part 1 solution", 514969},
 	}
-}
-
-func TestPart2Example(t *testing.T) {
-	input := []string{
-		"",
-	}
-
-	expected := 0
-
-	actual := solve2(input)
-
-	if actual != expected {
-		t.Errorf("Part2() = %d, expected %d", actual, expected)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Part1(); got != tt.want {
+				t.Errorf("Part1() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
 func TestPart2Solution(t *testing.T) {
-	expected := 0
-	actual := Part2()
+	tests := []struct {
+		name string
+		want int
+	}{
+		{"part 2 solution", 78915902},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Part2(); got != tt.want {
+				t.Errorf("Part2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
-	if actual != expected {
-		t.Errorf("Part2() = %d, expected %d", actual, expected)
+func TestPart1Examples(t *testing.T) {
+	type args struct {
+		input []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"part 1 example 1", args{[]string{
+			"467..114..",
+			"...*......",
+			"..35..633.",
+			"......#...",
+			"617*......",
+			".....+.58.",
+			"..592.....",
+			"......755.",
+			"...$.*....",
+			".664.598..",
+		}}, 4361},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := solve1(tt.args.input); got != tt.want {
+				t.Errorf("solve1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPart2Examples(t *testing.T) {
+	type args struct {
+		input []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"part 2 example 1", args{[]string{
+			"467..114..",
+			"...*......",
+			"..35..633.",
+			"......#...",
+			"617*......",
+			".....+.58.",
+			"..592.....",
+			"......755.",
+			"...$.*....",
+			".664.598..",
+		}}, 467835},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := solve2(tt.args.input); got != tt.want {
+				t.Errorf("solve2() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
